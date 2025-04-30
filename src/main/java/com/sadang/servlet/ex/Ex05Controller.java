271a48612b1a9948324be61548618566a44ee946
SaDang <SaDang@SaDang>
2025-04-30 18:29:03 +0900

    [SaDang]  - ex05controller 생성

---
+++ src/main/java/com/sadang/servlet/ex/Ex05Controller.java
@@ -0,0 +1,20 @@
+package com.sadang.servlet.ex;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex05Controller extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+		// head
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		// body
+
+	}
+
+}
